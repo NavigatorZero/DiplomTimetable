682fad13493a4d8b3cd5ef594b897db059f94184
Jameson <riothewizard@gmail.com>
2020-05-17 16:57:46 +0500

    logic fixes

---
+++ Timetable/MainApp/TablePreparation.py
@@ -0,0 +1,82 @@
+from datetime import date, timedelta
+
+from django.db.models import Sum
+
+from .Rasp import *
+from .UserInterface import *
+
+
+class TablePreparation:
+
+    def Erase(self):
+        MainTable.objects.all().delete()
+        group1 = studyPlanPE61.objects.all()
+        for x in range(len(group1)):
+            group1[x].remaningLectures = group1[x].hours
+            group1[x].save()
+
+        MainTableGroup2.objects.all().delete()
+        group2 = studyPlanGroup2.objects.all()
+        for x in range(len(group2)):
+            group2[x].remaningLectures = group2[x].hours
+            group2[x].save()
+
+    def Fill(self):
+
+        sdate = date(2020, 9, 1)  # start date
+        edate = date(2020, 9, 4)  # end date
+        delta = edate - sdate  # as timedelta
+        for i in range(delta.days + 1):
+            day = sdate + timedelta(days=i)
+
+            # заполняем первую группу
+            opa = []
+            for d in range(6):
+                test = MainTable()
+                test.Auditoriya = ""
+                test.NLecii = d + 1
+                test.Predmet = ""
+                test.Prepod = ""
+                test.Podgruppa = ""
+                test.vremya = day
+                opa.append(test)
+
+            MainTable.objects.bulk_create(opa)
+
+            # заполняем вторую группу
+            group2Main = []
+            for d in range(6):
+                test = MainTableGroup2()
+                test.Auditoriya = ""
+                test.NLecii = d + 1
+                test.Predmet = ""
+                test.Prepod = ""
+                test.Podgruppa = ""
+                test.vremya = day
+                group2Main.append(test)
+            MainTableGroup2.objects.bulk_create(opa)
+
+        semestrdays = 121  # кол-во дней в семестре
+        semestrHours = 726  # часов в семестре
+        subjectArray = CustomValues.objects.filter(alias="subject")
+
+        AllHours = studyPlanPE61.objects.aggregate(Sum('hours')).get('hours__sum',
+                                                                     0.00)  # кол-во часов одной группы в семестр по всем дисциплинам
+        mainTable1 = MainTable.objects.all()
+        mainTable2 = MainTableGroup2.objects.all()
+
+        PrepodArray = [teacher1.objects.all(), teacher2.objects.all(),
+                       teacher3.objects.all(),
+                       teacher4.objects.all(), teacher5.objects.all()]
+
+        Rasp.setTeacher(mainTable1, PrepodArray,
+                        1)  # Записываем преподователя в группу 1 на основе его выбора
+        Rasp.setTeacher(mainTable2, PrepodArray,
+                        2)  # Записываем преподователя в группу 2 на основе его выбора
+        for word in subjectArray:
+            if studyPlanPE61.objects.filter(subject=word.value).exists():
+                HoursByDiscipline = studyPlanPE61.objects.filter(subject=word.value).aggregate(Sum('hours')).get(
+                    'hours__sum',
+                    0.00)  # кол-во часов по дисциплине
+                Rasp.setSubject(mainTable1, studyPlanPE61.objects.filter(subject=word.value),
+                                HoursByDiscipline)  # Записываем предмет на основе кол-ва часов
--- Timetable/MainApp/views.py
+++ Timetable/MainApp/views.py
@@ -6,147 +6,45 @@ from django.contrib.auth import get_user_model
 from django.db import connection, transaction
 from django.db.models import Sum
 from django.shortcuts import render
+from . import UserInterface
+from . import TablePreparation
 
 from .Rasp import *
 from .models import *
 
 
+
 def Main_list(request):
-    listOfTeachers = []
     if request.method == 'POST':
+        preparations= TablePreparation.TablePreparation()
+        interface = UserInterface.UserInterface(request)
+
         variables = request.POST
 
-        if request.POST.get("lessonNumber") or request.POST.get("teacherRecordId"):
-            modelName = request.user.username
-            Model = apps.get_model('MainApp', modelName)
-
-            if request.POST.get("teacherRecordId"):
-                Model.objects.filter(id=request.POST['teacherRecordId']).delete()
-            else:
-                if Model.objects.filter(LessonNumber=request.POST.get('lessonNumber'), Date=request.POST.get('day')):
-                    print("who who who")
-                    return render(request, 'modal_table.html', {"teacherTable": Model.objects.all(), "error": True})
-                TeacherLesson = Model()
-                TeacherLesson.LessonNumber = request.POST['lessonNumber']
-                TeacherLesson.Date = request.POST['day']
-                TeacherLesson.teacher = request.user
-                TeacherLesson.save()
-
-            return render(request, 'modal_table.html', {"teacherTable": Model.objects.all()}, )
+        if request.POST.get("lessonNumber") or request.POST.get("teacherRecordId") or request.POST.get("userID"):
+            teacherTable = interface.LessonNumber()
+            return render(request, 'modal_table.html', {"teacherTable": teacherTable[0], "error": teacherTable[1]}, )
 
         if 'email' in variables:
-            UserModel = get_user_model()
-            user = UserModel.objects.get(email=variables['email'])
-            print(user)
-            user = authenticate(request, username=user, password=variables['password'])
-
-            if user is not None:
-                login(request, user)
-                if user.is_staff:
-                    modelName = user.username
-                    Model = apps.get_model('MainApp', modelName)
-
-                    teacherTable = Model.objects.all()
-                    Result = MainTable.objects.all()
-                    return render(request, 'test.html', {'posts': Result, 'teacherTable': teacherTable})
-            else:
-                print('no Auth')
+            interface.Email(variables)
 
         if 'logout' in variables:
             logout(request)
 
-        if 'erase' in variables:
-            MainTable.objects.all().delete()
-            group1 = studyPlanPE61.objects.all()
-            for x in range(len(group1)):
-                group1[x].remaningLectures = group1[x].hours
-                group1[x].save()
+        if request.method == 'POST' and 'scheduleDay' in request.POST:
+            oneDayRasp = interface.ScheduleDay()
+            return render(request, 'main_table.html',
+                          {'posts': oneDayRasp, 'dayRasp': request.POST['scheduleDay']})
 
-            MainTableGroup2.objects.all().delete()
-            group2 = studyPlanGroup2.objects.all()
-            for x in range(len(group2)):
-                group2[x].remaningLectures = group2[x].hours
-                group2[x].save()
+        if 'erase' in variables:
+            preparations.Erase()
 
         if 'fill' in variables:
+            preparations.Fill()
             # cursor = connection.cursor()
             # # Data modifying operation - commit required
             # # cursor.execute("UPDATE SQLITE_SEQUENCE SET seq = 0 WHERE name = 'MainApp_maintable'")
             # # transaction.commit()
-
-            sdate = date(2020, 9, 1)  # start date
-            edate = date(2020, 9, 4)  # end date
-
-            delta = edate - sdate  # as timedelta
-
-            for i in range(delta.days + 1):
-                day = sdate + timedelta(days=i)
-
-                # заполняем первую группу
-                opa = []
-                for d in range(6):
-                    test = MainTable()
-                    test.Auditoriya = ""
-                    test.NLecii = d + 1
-                    test.Predmet = ""
-                    test.Prepod = ""
-                    test.Podgruppa = ""
-                    test.vremya = day
-                    opa.append(test)
-
-                MainTable.objects.bulk_create(opa)
-
-                # заполняем вторую группу
-                group2Main = []
-                for d in range(6):
-                    test = MainTableGroup2()
-                    test.Auditoriya = ""
-                    test.NLecii = d + 1
-                    test.Predmet = ""
-                    test.Prepod = ""
-                    test.Podgruppa = ""
-                    test.vremya = day
-                    group2Main.append(test)
-                MainTableGroup2.objects.bulk_create(opa)
-
-            semestrdays = 121  # кол-во дней в семестре
-            semestrHours = 726  # часов в семестре
-            subjectArray = CustomValues.objects.filter(alias="subject")
-
-            AllHours = studyPlanPE61.objects.aggregate(Sum('hours')).get('hours__sum',
-                                                                         0.00)  # кол-во часов одной группы в семестр по всем дисциплинам
-            mainTable1 = MainTable.objects.all()
-            mainTable2 = MainTableGroup2.objects.all()
-
-            PrepodArray = [teacher1.objects.all(), teacher2.objects.all(),
-                           teacher3.objects.all(),
-                           teacher4.objects.all(), teacher5.objects.all()]
-
-            Rasp.setTeacher(mainTable1, PrepodArray,
-                            1)  # Записываем преподователя в группу 1 на основе его выбора
-            Rasp.setTeacher(mainTable2, PrepodArray,
-                            2)  # Записываем преподователя в группу 2 на основе его выбора
-            for word in subjectArray:
-                if studyPlanPE61.objects.filter(subject=word.value).exists():
-                    HoursByDiscipline = studyPlanPE61.objects.filter(subject=word.value).aggregate(Sum('hours')).get(
-                        'hours__sum',
-                        0.00)  # кол-во часов по дисциплине
-                    Rasp.setSubject(mainTable1, studyPlanPE61.objects.filter(subject=word.value),
-                                    HoursByDiscipline)  # Записываем предмет на основе кол-ва часов
-                if studyPlanGroup2.objects.filter(subject=word.value).exists():
-                    HoursByDiscipline = studyPlanGroup2.objects.filter(subject=word.value).aggregate(Sum('hours')).get(
-                        'hours__sum',
-                        0.00)  # кол-во часов по дисциплине
-                    Rasp.setSubject(mainTable2, studyPlanGroup2.objects.filter(subject=word.value),
-                                    HoursByDiscipline)  # Записываем предмет на основе кол-ва часов
-        if request.method == 'POST' and 'scheduleDay' in request.POST:
-            print(request.POST['scheduleDay'])
-            modelName = request.POST['group']
-            Model = apps.get_model('MainApp', modelName)
-
-            oneDayRasp = Model.objects.filter(vremya=request.POST['scheduleDay'])
-
-            return render(request, 'main_table.html', {'posts': oneDayRasp, 'dayRasp': request.POST['scheduleDay']})
 
     Result = MainTable.objects.all()
 
